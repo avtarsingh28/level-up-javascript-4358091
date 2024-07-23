e52276bc7e91aa7d485dae85df18fdad341f515c
avtarsingh28 <singhavtar28@gmail.com>
2024-07-23 13:30:34 +0530

    Update 01_01_available_books.js

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,29 @@
-// Write your code here
+class Book {
+  constructor(title, author, ISBN, numCopies) {
+    this.title = title;
+    this.author = author;
+    this.ISBN = ISBN;
+    this.numCopies = numCopies;
+  }
+ 
+  get availability() {
+    return this.getAvailability();
+  }
+
+  getAvailability() {
+    if (this.numCopies === 0) {
+      return "Out of stock";
+    } else if (this.numCopies < 10) {
+      return "Low stock";
+    }
+    return "In stock";
+  }
+  sell(numCopiesSold = 1) {
+    this.numCopies -= numCopiesSold;
+  }
+  
+  restock(numCopiesStocked = 5) {
+    this.numCopies += numCopiesStocked;
+  }
+
+}
